59b4684fd7671096d4c39a884f033df7da44771c
Giselle Raposo <raposo_gigi@icloud.com>
2021-07-30 12:17:16 -0400

    added end.js, final css, now looking for a bug

--- game.js
+++ game.js
@@ -68,7 +68,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('end.html')
     }
 
     questionCounter++
@@ -128,78 +128,3 @@ startGame()
 
 
 
-//         
-
-//     }
-
-// ];
-
-// const quiz = document.getElementById('quiz');
-// const answerEls = document.querySelectorAll
-// ('.answer');
-// const questionEl = document.getElementById
-// ('question');
-// const a_text = document.getElementById('a_text');
-// const b_text = document.getElementById('b_text');
-// const c_text = document.getElementById('c_text');
-// const d_text = document.getElementById('d_text');
-// const e_text = document.getElementById('e_text');
-// const submitBtn = document.getElementById
-// ('submit');
-
-// let currentQuiz = 0;
-// let score = 0
-
-// loadQuiz();
-
-// function loadQuiz() {
-//     deselectAnswers();
-    
-//     const currentQuizData = quizData[currentQuiz];
-    
-//     questionEl.innerText = currentQuizData.
-//     question;
-//     a_text.innerText = currentQuizData.a;
-//     b_text.innerText = currentQuizData.b;
-//     c_text.innerText = currentQuizData.c;
-//     d_text.innerText = currentQuizData.d;
-//     e_text.innerText = currentQuizData.e;
-// }
-
-// function getSelected() {
-//     let answer = undefined
-
-//     answerEls.forEach((answerEl) => {
-//         if(answerEl.checked) {
-//             answer = answerEl.id;
-//         }
-//     });
-
-//     return answer;
-// }
-// function deselectAnswers() {
-//     answerEls.forEach((answerEl) => {
-//         answerEl.checked = false;
-//     });
-// }
-
-// submitBtn.addEventListener('click', () => {
-//     //check answer
-//     const answer = getSelected();
-
-//     if (answer) {
-//         if (answer === quizData[currentQuiz].
-//         correct) {
-//             score++;
-//         }
-        
-//         currentQuiz++;
-//         if (currentQuiz < quizData.length) {
-//             loadQuiz();  
-//         } else {
-//                 quiz.innerHTML = `<h2>Your correct answers ${score}/${quizData.length}</h2>`;
-//         }
-        
-        
-//     }
-// });
